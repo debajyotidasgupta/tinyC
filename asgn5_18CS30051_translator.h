24b6246cc39c57e257581175ff43807a46b82585
debajyotidasgupta <debajyotidasgupta6@gmail.com>
2020-10-24 13:54:28 +0530

    spaces

--- asgn5_18CS30051_translator.h
+++ asgn5_18CS30051_translator.h
@@ -91,10 +91,10 @@ public:
 //--------------------------------------------------//
 class symType {
 public:
-    string type;																	// Type of the element
-    int width;																		// Size of array / number of elements (in case of arrays)
-    symType* ptr;																	// pointer to type of the array or pointer for pinter type, 1D and 2D arrays
-    symType(string type, symType* ptr = NULL, int width = 1);						// Constructor
+    string type;                                                                     // Type of the element
+    int width;                                                                       // Size of array / number of elements (in case of arrays)
+    symType* ptr;                                                                    // pointer to type of the array or pointer for pinter type, 1D and 2D arrays
+    symType(string type, symType* ptr = NULL, int width = 1);                        // Constructor
 };
 
 //----------------------------------------------------------------------//
@@ -102,15 +102,15 @@ public:
 //----------------------------------------------------------------------//
 class symElem {
 public:
-    string name;																	// Name of the symbol
-    symType *type;																	// Type of the Symbol
-    string initial_value;															// Symbol initial valus (if any)
-    int size;																		// Size of the symbol
-    int offset;																		// Offset of symbol
-    symTable* nested;																// Pointer to nested symbol table
+    string name;                                                                     // Name of the symbol
+    symType *type;                                                                   // Type of the Symbol
+    string initial_value;                                                            // Symbol initial valus (if any)
+    int size;                                                                        // Size of the symbol
+    int offset;                                                                      // Offset of symbol
+    symTable* nested;                                                                // Pointer to nested symbol table
 
-    symElem (string name, string t, symType* ptr = NULL, int width = 0);	 		//constructor declaration
-    symElem* update(symType *t); 													// A method to update different fields of an existing entry.
+    symElem (string name, string t, symType* ptr = NULL, int width = 0);             //constructor declaration
+    symElem* update(symType *t);                                                     // A method to update different fields of an existing entry.
 };
 
 //----------------------------------------------------------//
@@ -118,15 +118,15 @@ public:
 //----------------------------------------------------------//
 class quadArray {
 public:
-    vector <quad> array; 		                									// Array of quad elements
-    void print ();																	// Print the quad array
+    vector <quad> array;                                                             // Array of quad elements
+    void print ();                                                                   // Print the quad array
 };
 
 //--------------------------------------------------------------//
 //          Defination of statements with dangling exits        //
 //--------------------------------------------------------------//
 struct statement {
-    vector<int> nextlist;															// Nextlist for statement
+    vector<int> nextlist;                                                            // Nextlist for statement
 };
 
 //--------------------------------------------------------------//
@@ -134,37 +134,37 @@ struct statement {
 //--------------------------------------------------------------//
 struct array {
     string cat;
-    symElem* ptr;																	// Temporary used for computing array address
-    symElem* array;																	// Pointer to symbol table
-    symType* type;																	// type of the subarray generated
+    symElem* ptr;                                                                    // Temporary used for computing array address
+    symElem* array;                                                                  // Pointer to symbol table
+    symType* type;                                                                   // type of the subarray generated
 };
 
 //------------------------------------------------------------------//
 //          Overloaded emit function used by the parser             //
 //------------------------------------------------------------------//
-void emit(string op, string result, int arg1, string arg2 = "");		  			//emits for adding quads to quadArray (arg1 is int)
-void emit(string op, string result, float arg1, string arg2 = "");        			//emits for adding quads to quadArray (arg1 is float)
-void emit(string op, string result, string arg1="", string arg2 = "");    			//emits for adding quads to quadArray
+void emit(string op, string result, int arg1, string arg2 = "");                     //emits for adding quads to quadArray (arg1 is int)
+void emit(string op, string result, float arg1, string arg2 = "");                   //emits for adding quads to quadArray (arg1 is float)
+void emit(string op, string result, string arg1="", string arg2 = "");               //emits for adding quads to quadArray
 
 //----------------------------------------------------------------------//
 //          Other global functions required for TAC generation          //
 //----------------------------------------------------------------------//
-symElem* conv (symElem*, string);													// TAC for Type conversion in program
-bool typecheck(symElem* &s1, symElem* &s2);											// Checks if two symbols have same type
-bool typecheck(symType* t1, symType* t2);											// Checks if two symtype objects have same type
+symElem* conv (symElem*, string);                                                    // TAC for Type conversion in program
+bool typecheck(symElem* &s1, symElem* &s2);                                          // Checks if two symbols have same type
+bool typecheck(symType* t1, symType* t2);                                            // Checks if two symtype objects have same type
 
-symElem* gentemp (symType* t);														// Generate a temporary variable and insert it in current symbol table
+symElem* gentemp (symType* t);                                                       // Generate a temporary variable and insert it in current symbol table
 
-int size_type (symType*);															// Calculate size of any symbol type
-string print_type(symType*);														// For printing type of symbol recursive printing of type
+int size_type (symType*);                                                            // Calculate size of any symbol type
+string print_type(symType*);                                                         // For printing type of symbol recursive printing of type
 
-vector<int> makelist (int i);							       						// Make a new list contaninig an integer
-void backpatch (vector<int> lst, int i);											// Backpatch all the elements of the list with the given address
-vector<int> merge (vector<int> &lst1, list <int> &lst2);							// Merge two lists into a single list
+vector<int> makelist (int i);                                                        // Make a new list contaninig an integer
+void backpatch (vector<int> lst, int i);                                             // Backpatch all the elements of the list with the given address
+vector<int> merge (vector<int> &lst1, list <int> &lst2);                             // Merge two lists into a single list
 
-expression* convertInt2Bool (expression*);											// convert any expression (int) to bool
-expression* convertBool2Int (expression*);											// convert bool to expression (int)
-void changeTable (symTable* newtable);               								// For changing the current sybol table
-int nextinstr();									 								// Returns the next instruction number
+expression* convertInt2Bool (expression*);                                           // convert any expression (int) to bool
+expression* convertBool2Int (expression*);                                           // convert bool to expression (int)
+void changeTable (symTable* newtable);                                               // For changing the current sybol table
+int nextinstr();                                                                     // Returns the next instruction number
 
 #endif
